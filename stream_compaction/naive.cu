3757d5d06f6803480914b90da61a16882f4acb01
asalexan <80473081+asalexan@users.noreply.github.com>
2021-09-16 15:15:16 -0400

    Draft naive scan implementation

--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -3,6 +3,12 @@
 #include "common.h"
 #include "naive.h"
 
+#define blockSize 128;
+dim3 threadsPerBlock(blockSize);
+
+int *dev_data1;
+int *dev_data2;
+
 namespace StreamCompaction {
     namespace Naive {
         using StreamCompaction::Common::PerformanceTimer;
@@ -11,15 +17,46 @@ namespace StreamCompaction {
             static PerformanceTimer timer;
             return timer;
         }
-        // TODO: __global__
+        __global__ void kernScan(int n, int *odata, int *idata, int d){
+            int index = (blockDim.x * blockIdx.x) + threadIdx.x;
+            
+            int offset = pow(2, d-1);
+            if (index >= offset){
+                odata[index] = odata[index - offset] + idata[index];  
+            }
+            else{
+                odata[index] = idata[index];   
+            }
+        }
 
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
+            
+            // allocate memory
+            dim3 fullBlocksPerGrid((n + blockSize - 1) / blockSize);
+            cudaMalloc((void**)&dev_data1, n * sizeof(int));
+            checkCUDAErrorWithLine("cudaMalloc dev_data1 failed!");
+            cudaMalloc((void**)&dev_data2, n * sizeof(int));
+            
             timer().startGpuTimer();
-            // TODO
+           
+            // map bools to ints
+            kernMapToBoolean<<<fullBlocksPerGrid, threadsPerBlock>>>(n, idata, dev_data1);
+            
+            // for log iterations, perform scan
+            for (int d = 1; d < ilog2ceil(n); d++){
+                kernScan<<<fullBlocksPerGrid, threadsPerBlock>>>(n, dev_data2, dev_data1, d);
+                
+                // ping-pong
+                int *tmp = dev_data1;
+                dev_data1 = dev_data2;
+                dev_data2 = tmp;
+            }
             timer().endGpuTimer();
+            
+            // cudaMemcpy dev_data2 to odata (device -> host)
         }
     }
 }
