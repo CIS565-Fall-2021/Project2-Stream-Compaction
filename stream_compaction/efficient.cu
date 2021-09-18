fa47dc62c7abfa17624be172c1147f0916a1aaf9
matt elser <elserm@gmail.com>
2021-09-18 12:56:11 -0400

    got work efficient scan working for powers of 2

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -20,19 +20,27 @@ namespace StreamCompaction {
         __global__ void kernUpSweep(int n, int *odata, int d) {
 
             int index = (blockIdx.x * blockDim.x) + threadIdx.x;
-            int k = index * (1 << (d + 1));
-
             if (index > n) {
                 return;
             }
 
+            int k = index * (1 << (d + 1));
+
 			odata[k + ((1<<(d+1))-1)] = odata[k + (1 << d) - 1] + odata[k + (1 << (d+1)) - 1];
             __syncthreads();
-            return;
         }
 
-        __global__ void kernDownSweep(int n, int *odata, const int *idata, int d) {
+        __global__ void kernDownSweep(int n, int *odata, int d) {
+            int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+            if (index > n) {
+                return;
+            }
 
+            int k = index * (1 << (d + 1));
+            int t = odata[k + (1 << d) - 1];
+            odata[k + (1 << d) - 1] = odata[k + (1 << (d + 1)) - 1];
+            odata[k + (1 << (d + 1)) - 1] += t;
+            __syncthreads();
         }
 
         /**
@@ -54,33 +62,30 @@ namespace StreamCompaction {
             // --- up sweep ---
             for (int d = 0; d < log2(paddedN); d++) {
 				int numThreads = ((paddedN - 1) / (1 << (d + 1))) + 1;
-                //numthreads = numThreads > 0 ? numthreads : 1;
-                printf("blocksize: %i\t numthreads: %i\n", BLOCKSIZE, numThreads);
 				kernUpSweep <<<numThreads, BLOCKSIZE>>> (numThreads, dev_writeable, d);
 				checkCUDAErrorFn("upsweep failed", "efficent.cu", 50);
 				cudaDeviceSynchronize();
 				cudaMemcpy(odata, dev_writeable, n * sizeof(int), cudaMemcpyDeviceToHost);
             }
 
-
             // --- down sweep ---
             // insert 0 at the end of the in-progress output
-            //cudaMemcpy(dev_writeable + n - 1, { 0 }, sizeof(int), cudaMemcpyHostToDevice);
+            int ZERO = 0;
+            cudaMemcpy(dev_writeable + n - 1, &ZERO, sizeof(int), cudaMemcpyHostToDevice);
 			cudaMemcpy(odata, dev_writeable, n * sizeof(int), cudaMemcpyDeviceToHost);
-            for (int d = 0; d < log2(paddedN); d++) {
-				//int numThreads = (paddedN - 1) / (1 << d + 1);
-				//kernDownSweep <<<numThreads, BLOCKSIZE>>> (numThreads, dev_writeable, dev_readable, d);
-				//checkCUDAErrorFn("downSweep failed", "efficent.cu", 70);
-				//cudaDeviceSynchronize();
+            for (int d = log2(paddedN - 1); d >= 0; d--) {
+				int numThreads = ((paddedN - 1) / (1 << (d + 1))) + 1;
+				kernDownSweep <<<numThreads, BLOCKSIZE>>> (numThreads, dev_writeable, d);
+				checkCUDAErrorFn("downsweep failed", "efficent.cu", 70);
+				cudaDeviceSynchronize();
+				cudaMemcpy(odata, dev_writeable, n * sizeof(int), cudaMemcpyDeviceToHost);
             }
 
             timer().endGpuTimer();
 
             // this is an exclusive scan, so the first elem should be 0
             // and we shift everything (except the last elem) one index right
-            odata[0] = 0; // TODO try removing this
-            cudaMemcpy(odata+1, dev_writeable, (n-1) * sizeof(int), cudaMemcpyDeviceToHost);
-            cudaFree(dev_readable);
+            cudaMemcpy(odata, dev_writeable, n * sizeof(int), cudaMemcpyDeviceToHost);
             cudaFree(dev_writeable);
 
         }
