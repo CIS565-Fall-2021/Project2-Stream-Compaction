75e69001ac8f03d0b1444e2260a113e056aa3c77
Zhihao Ruan <ruanzh@seas.upenn.edu>
2021-09-18 03:54:25 -0400

    enable printing of naive scan

--- src/main.cpp
+++ src/main.cpp
@@ -59,7 +59,7 @@ int main(int argc, char *argv[]) {
   printElapsedTime(
       StreamCompaction::Naive::timer().getGpuElapsedTimeForPreviousOperation(),
       "(CUDA Measured)");
-  // printArray(SIZE, c, true);
+  printArray(SIZE, c, true);
   printCmpResult(SIZE, b, c);
 
   /* For bug-finding only: Array of 1s to help find bugs in stream compaction or
@@ -73,7 +73,7 @@ int main(int argc, char *argv[]) {
   printElapsedTime(
       StreamCompaction::Naive::timer().getGpuElapsedTimeForPreviousOperation(),
       "(CUDA Measured)");
-  // printArray(SIZE, c, true);
+  printArray(SIZE, c, true);
   printCmpResult(NPOT, b, c);
 
   zeroArray(SIZE, c);
