34b38e6f49c173e8d342255dcc9d5faa2b1c5796
Aditya Hota <12516225+adityahota@users.noreply.github.com>
2021-09-19 15:36:45 -0400

    [1] Finish CPU implementations

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -19,7 +19,18 @@ namespace StreamCompaction {
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+            // DONE-Part 1
+            if (n < 0)
+            {
+                timer().endCpuTimer();
+                return;
+            }
+
+            odata[0] = 0;
+            for (int i = 1; i < n; i++)
+            {
+                odata[i] = odata[i - 1] + idata[i - 1];
+            }
             timer().endCpuTimer();
         }
 
@@ -30,9 +41,26 @@ namespace StreamCompaction {
          */
         int compactWithoutScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+            // DONE-Part 1
+            if (n < 0)
+            {
+                timer().endCpuTimer();
+                return 0;
+            }
+
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int data = idata[i];
+                if (data != 0)
+                {
+                    odata[count] = data;
+                    count++;
+                }
+            }
+
             timer().endCpuTimer();
-            return -1;
+            return count;
         }
 
         /**
@@ -42,9 +70,46 @@ namespace StreamCompaction {
          */
         int compactWithScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+            // DONE-Part 1
+            if (n < 0)
+            {
+                timer().endCpuTimer();
+                return 0;
+            }
+
+            // Transform input into binary array
+            int* valid_indices = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                valid_indices[i] = (idata[i] == 0) ? 0 : 1;
+            }
+
+            // Run scan algorithm on valid index data
+            // Please note that I could not call the scan function as it starts
+            // the timer again and causes the program to crash.
+            int *output_indices = new int[n];
+            output_indices[0] = 0;
+            for (int i = 1; i < n; i++)
+            {
+                output_indices[i] = output_indices[i - 1] + valid_indices[i - 1];
+            }
+
+            // Write valid data to output based on indices computed in scan
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (valid_indices[i] == 1)
+                {
+                    odata[output_indices[i]] = idata[i];
+                    count++;
+                }
+            }
+
+            delete[] valid_indices;
+            delete[] output_indices;
+
             timer().endCpuTimer();
-            return -1;
+            return count;
         }
     }
 }
