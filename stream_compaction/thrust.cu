90b98275b34926eb37ebd32c2ef51427d251c8c5
matt elser <elserm@gmail.com>
2021-09-18 22:56:12 -0400

    got thrust working

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -19,10 +19,15 @@ namespace StreamCompaction {
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
-            // TODO use `thrust::exclusive_scan`
-            // example: for device_vectors dv_in and dv_out:
-            // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+            thrust::device_vector<int> dev_odata(odata, odata + n);
+            thrust::device_vector<int> dev_idata(idata, idata + n);
+
+            thrust::exclusive_scan(dev_idata.begin(), dev_idata.end(), dev_odata.begin());
+            
             timer().endGpuTimer();
+
+            thrust::copy(dev_odata.begin(), dev_odata.end(), odata);
+
         }
     }
 }
