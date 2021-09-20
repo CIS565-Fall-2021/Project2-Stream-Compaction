68585dc4fbc8df821f08e46e2b2bf1d24e8e05d0
Aditya Hota <12516225+adityahota@users.noreply.github.com>
2021-09-20 12:05:47 -0400

    [3.1 + 3.2] Finish work efficient implementation

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -3,6 +3,8 @@
 #include "common.h"
 #include "efficient.h"
 
+#define BLOCK_SIZE 128
+
 namespace StreamCompaction {
     namespace Efficient {
         using StreamCompaction::Common::PerformanceTimer;
@@ -13,12 +15,77 @@ namespace StreamCompaction {
         }
 
         /**
+         * Performs an in-place up sweep on the data.
+         */
+        __global__ void kernUpSweep(int N, int d, int *data) {
+            int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+            if (index >= N)
+            {
+                return;
+            }
+
+            if (index % (1 << (d + 1)) == 0)
+            {
+                data[index + (1 << (d + 1)) - 1] += data[index + (1 << d) - 1];
+            }
+        }
+
+        /**
+         * Performs an in-place down sweep on the data.
+         */
+        __global__ void kernDownSweep(int N, int d, int *data) {
+            int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+            if (index >= N)
+            {
+                return;
+            }
+
+            if (index % (1 << (d + 1)) == 0)
+            {
+                int t = data[index + (1 << d) - 1];
+                data[index + (1 << d) - 1] = data[index + (1 << (d + 1)) - 1];
+                data[index + (1 << (d + 1)) - 1] += t;
+            }
+        }
+
+        /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
-        void scan(int n, int *odata, const int *idata) {
+        void scan(int N, int *odata, const int *idata) {
+
+            // Done - Part 3.1
+            int tree_depth = ilog2ceil(N);
+            int data_length = 1 << tree_depth;
+            int data_bytes = data_length * sizeof(int);
+
+            int* dev_data;
+            cudaMalloc((void**)&dev_data, data_bytes);
+            cudaMemset(dev_data, 0, data_bytes);
+            cudaMemcpy(dev_data, idata, N * sizeof(int), cudaMemcpyHostToDevice);
+
             timer().startGpuTimer();
-            // TODO
+            dim3 fullBlocksPerGrid(data_length + BLOCK_SIZE - 1 / BLOCK_SIZE);
+
+            // Perform up sweep
+            for (int d = 0; d < tree_depth - 1; d++)
+            {
+                kernUpSweep <<<fullBlocksPerGrid, BLOCK_SIZE>>>(data_length, d, dev_data);
+            }
+
+            // Zero out the root
+            cudaMemset(dev_data + data_length - 1, 0, sizeof(int));
+
+            // Perform down sweep
+            for (int d = tree_depth - 1; d >= 0; d--)
+            {
+                kernDownSweep <<<fullBlocksPerGrid, BLOCK_SIZE>>>(data_length, d, dev_data);
+            }
+
             timer().endGpuTimer();
+
+            // Copy scanned array to host
+            cudaMemcpy(odata, dev_data, N * sizeof(int), cudaMemcpyDeviceToHost);
+            cudaFree(dev_data);
         }
 
         /**
@@ -31,10 +98,66 @@ namespace StreamCompaction {
          * @returns      The number of elements remaining after compaction.
          */
         int compact(int n, int *odata, const int *idata) {
+            int tree_depth = ilog2ceil(n);
+            int data_length = 1 << tree_depth;
+            int data_bytes = data_length * sizeof(int);
+            int num_elts = 0;
+
+            int *dev_orig_data;
+            cudaMalloc((void**)&dev_orig_data, data_bytes);
+            cudaMemset(dev_orig_data, 0, data_bytes);
+            cudaMemcpy(dev_orig_data, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+
+            int *dev_output;
+            cudaMalloc((void**)&dev_output, n * sizeof(int));
+
+            int *dev_valid_indices;
+            cudaMalloc((void**)&dev_valid_indices, data_bytes);
+
+            int *dev_scanned_indices;
+            cudaMalloc((void**)&dev_scanned_indices, data_bytes);
+
             timer().startGpuTimer();
-            // TODO
+            // Done-Part 3.2
+            dim3 fullBlocksPerGrid((n + BLOCK_SIZE - 1) / BLOCK_SIZE);
+
+            // Transform input into binary array
+            StreamCompaction::Common::kernMapToBoolean<<<fullBlocksPerGrid, BLOCK_SIZE>>>(data_length, dev_valid_indices, dev_orig_data);
+            cudaMemcpy(dev_scanned_indices, dev_valid_indices, data_bytes, cudaMemcpyDeviceToDevice);
+
+            // Perform up sweep
+            for (int d = 0; d < tree_depth - 1; d++)
+            {
+                kernUpSweep<<<fullBlocksPerGrid, BLOCK_SIZE>>>(data_length, d, dev_scanned_indices);
+            }
+
+            // Zero out the root
+            cudaMemset(dev_scanned_indices + data_length - 1, 0, sizeof(int));
+
+            // Perform down sweep
+            for (int d = tree_depth - 1; d >= 0; d--)
+            {
+                kernDownSweep<<<fullBlocksPerGrid, BLOCK_SIZE>>>(data_length, d, dev_scanned_indices);
+            }
+
+            // Write valid data to output based on indices computed in scan (scatter)
+            StreamCompaction::Common::kernScatter<<<fullBlocksPerGrid, BLOCK_SIZE>>>(data_length, dev_output, dev_orig_data, dev_valid_indices, dev_scanned_indices);
+
             timer().endGpuTimer();
-            return -1;
+
+            // Copy filtered data to host
+            cudaMemcpy(odata, dev_output, n * sizeof(int), cudaMemcpyDeviceToHost);
+
+            // Copy number of elements
+            cudaMemcpy(&num_elts, dev_scanned_indices + data_length - 1, sizeof(int), cudaMemcpyDeviceToHost);
+
+            // Free all buffers
+            cudaFree(dev_orig_data);
+            cudaFree(dev_valid_indices);
+            cudaFree(dev_scanned_indices);
+            cudaFree(dev_output);
+
+            return num_elts;
         }
     }
 }
