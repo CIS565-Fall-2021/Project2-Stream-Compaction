ed0f9a1111ecb8f6a405afca88f88cba9818b7a6
Scoutydren <gubeini@seas.upenn.edu>
2021-09-20 11:49:36 -0400

    naive ok

--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -11,15 +11,49 @@ namespace StreamCompaction {
             static PerformanceTimer timer;
             return timer;
         }
-        // TODO: __global__
+
+        __global__ void kernScan(int n, int d, const int* idata, int* odata) {
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+            if (index >= n) {
+                return;
+            }
+            int pow2d = 1 << (d - 1);
+            if (index >= pow2d) {
+                odata[index] = idata[index] + idata[index - pow2d];
+            }
+        }
 
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
+            int* deviceIn;
+            int* deviceOut;
+            cudaMalloc((void**)&deviceIn, n * sizeof(int));
+            cudaMalloc((void**)&deviceOut, n * sizeof(int));
+            cudaMemcpy(deviceIn, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+            cudaMemcpy(deviceOut, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+            
             timer().startGpuTimer();
-            // TODO
+            int blockSize = 128;
+           
+            dim3 fullBlocksPerGrid((n + blockSize - 1) / blockSize);
+
+            for (int d = 1; d <= ilog2ceil(n); d++) {
+                kernScan << < fullBlocksPerGrid, blockSize >> > (n, d, deviceIn, deviceOut);
+                checkCUDAError("kernScan failed");
+                cudaMemcpy(deviceIn, deviceOut, sizeof(int) * n, cudaMemcpyDeviceToDevice);
+            }
             timer().endGpuTimer();
+
+            odata[0] = 0;
+         
+            //shift
+            cudaMemcpy(odata + 1, deviceIn, sizeof(int) * (n - 1), cudaMemcpyDeviceToHost);
+
+            checkCUDAError("cudaMemcpy back failed!");
+            cudaFree(deviceIn);
+            cudaFree(deviceOut);
         }
     }
 }
