551e01bc4be7d8bc65b69aaea1e5357363b1e9b5
Nithin Pranesh <nithinpra7@gmail.com>
2021-09-21 16:40:06 -0400

    wrapped thrust::scan_exclusive

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -19,9 +19,26 @@ namespace StreamCompaction {
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
-            // TODO use `thrust::exclusive_scan`
-            // example: for device_vectors dv_in and dv_out:
-            // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+
+            int* dev_in;
+            cudaMalloc((void**)&dev_in, sizeof(int) * n);
+            checkCUDAError("cudaMalloc dev_in failed!");
+
+            int* dev_out;
+            cudaMalloc((void**)&dev_out, sizeof(int) * n);
+            checkCUDAError("cudaMalloc dev_out failed!");
+
+            cudaMemcpy(dev_in, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+            checkCUDAError("cudaMemcpy to device failed!");
+
+            thrust::device_ptr<int> dev_thrust_in = thrust::device_pointer_cast(dev_in);
+            thrust::device_ptr<int> dev_thrust_out = thrust::device_pointer_cast(dev_out);
+
+            thrust::exclusive_scan(dev_thrust_in, dev_thrust_in + n, dev_thrust_out);
+
+            cudaMemcpy(odata, dev_out, sizeof(int) * n, cudaMemcpyDeviceToHost);
+            checkCUDAError("cudaMemcpy from device failed!");
+
             timer().endGpuTimer();
         }
     }
