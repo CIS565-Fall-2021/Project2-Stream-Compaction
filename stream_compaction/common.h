6bf6883e88085e424fc73c1a690d034543eb1063
Stephen Lee <slee11030@users.noreply.github.com>
2021-09-21 17:51:27 -0400

    update blockSize and number of elements to be processed

--- stream_compaction/common.h
+++ stream_compaction/common.h
@@ -12,7 +12,7 @@
 
 #define FILENAME (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 #define checkCUDAError(msg) checkCUDAErrorFn(msg, FILENAME, __LINE__)
-#define blockSize 512
+#define blockSize 128
 
 /**
  * Check for CUDA errors; print and exit if there was a problem.
