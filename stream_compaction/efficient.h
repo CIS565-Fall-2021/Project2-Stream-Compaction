feec733f8d23ae254861849f3a1aae242052305a
kaan <39895510+kaan9@users.noreply.github.com>
2021-12-14 19:11:25 -0500

    implemented basic naive shared scan

--- stream_compaction/efficient.h
+++ stream_compaction/efficient.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include "common.h"
+#include "cVec.h"
 
 namespace StreamCompaction {
     namespace Efficient {
         StreamCompaction::Common::PerformanceTimer& timer();
+
+	/* in-place scan over device array, doesn't start GPU Timer and assumes input is power of 2 */
+	void scan_dev(int N, cu::cVec<int>* dev_data);
 
         void scan(int n, int *odata, const int *idata);
 
--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -31,6 +31,8 @@ namespace Naive {
 			out[idx] = in[idx];
 	}
 
+
+
 	/**
 	 * Performs prefix-sum (aka scan) on idata, storing the result into odata.
 	 */
@@ -58,5 +60,62 @@ namespace Naive {
 		cu::copy(odata+1, data_in.ptr(), n-1);
 		odata[0] = 0;
 	}
+
+	
+	template <typename T>
+	__global__ void kern_shared_scan(int n, const T* __restrict__ in, T* __restrict__ out)
+	{
+		extern __shared__ T tmp[];
+		T *b1 = tmp, *b2 = tmp + blockSize, *b3; /* double buffers for swapping */
+		int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+		if (idx >= n)
+			return;
+
+		b1[idx] = idx > 0 ? in[idx-1] : 0;
+		__syncthreads();
+		
+		#pragma unroll
+		for (int d = 1; d < blockSize; d *= 2) { /* p is for swapping between shared mem */
+			b3 = b1, b2 = b1, b2 = b3; /* swap b1, b2*/
+			if (idx >= d)
+				b1[idx] += b2[idx - d];
+			else
+				b1[idx] = b2[idx];
+			__syncthreads();
+		}
+		out[idx] = b1[idx];
+	}
+
+	template <typename T>
+	__global__ void kern_add(int n, T* x, const T* val)
+	{
+		int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+		if (idx >= n)
+			return;
+		
+		x[idx] += *val;
+	}
+
+
+	void shared_scan(int n, int* odata, const int* idata)
+	{
+		int block_count = (n+blockSize - 1) / blockSize;
+		int N = block_count * blockSize;
+	
+		cu::cVec<int> in(n, idata, N), out(N);
+
+		timer().startGpuTimer();
+		
+		kern_shared_scan<<<block_count, blockSize, 2 * blockSize * sizeof(int)>>>(n, in.raw_ptr(), out.raw_ptr());
+		
+		for (int i = 1; i < block_count; i++) {
+			kern_shared_scan<<<block_count - i, blockSize>>>(n - i * blockSize, out.raw_ptr() + blockSize * i, out.raw_ptr() + blockSize * i - 1);
+		}
+
+		timer().endGpuTimer();
+
+		cu::copy(odata, out.ptr(), n);
+	}
 }
 }
+
